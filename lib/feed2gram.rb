00e02c125ad0f4e5f02988cfd58c3d159da83bab
Justin Searls <searls@gmail.com>
2023-10-29 09:13:41 -0400

    $ bundle gem feed2gram

---
+++ lib/feed2gram.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "feed2gram/version"
+
+module Feed2gram
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/feed2gram/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Feed2gram
+  VERSION = "0.1.0"
+end
