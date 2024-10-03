090f7d2113221ee9c64350dc5a7358db92b0fc22
Justin Searls <searls@gmail.com>
2024-10-03 17:02:05 -0400

    enable API use for the main thing the gem does. Don't call these methods if your name doesn't rhyme with Pearls

--- lib/feed2gram/publishes_posts.rb
+++ lib/feed2gram/publishes_posts.rb
@@ -31,6 +31,79 @@ module Feed2Gram
       }
     end
 
+    # Pseudo-public "API mode"
+    def create_single_media_container(post, config)
+      media = post.medias.first
+      Http.post("/#{config.instagram_id}/media", {
+        :media_type => post.media_type,
+        :caption => post.caption,
+        :access_token => config.access_token,
+        :cover_url => media.cover_url,
+        media.video? ? :video_url : :image_url => media.url
+      }.compact)[:id]
+    end
+
+    # Pseudo-public "API mode"
+    def create_carousel_media_container(media, config)
+      res = Http.post("/#{config.instagram_id}/media", {
+        :media_type => media.media_type,
+        :is_carousel_item => true,
+        :access_token => config.access_token,
+        media.video? ? :video_url : :image_url => media.url
+      }.compact)
+      res[:id]
+    end
+
+    # Pseudo-public "API mode"
+    def create_carousel_container(post, media_containers, config)
+      Http.post("/#{config.instagram_id}/media", {
+        caption: post.caption,
+        media_type: post.media_type,
+        children: media_containers.join(","),
+        access_token: config.access_token
+      })[:id]
+    end
+
+    # Pseudo-public "API mode"
+    def upload_finished?(container_id, config)
+      res = Http.get("/#{container_id}", {
+        fields: "status_code,status",
+        access_token: config.access_token
+      })
+
+      if res[:status_code] == "FINISHED"
+        true
+      elsif res[:status_code] == "IN_PROGRESS"
+        false
+      else
+        raise <<~MSG
+          Unexpected status code (#{res[:status_code]}) uploading container: #{container_id}"
+
+          API sent back this: #{res[:status]}
+
+          Error codes can be looked up here: https://developers.facebook.com/docs/instagram-platform/instagram-graph-api/reference/error-codes/
+        MSG
+      end
+    end
+
+    # Pseudo-public "API mode"
+    def publish_container(container_id, config)
+      res = Http.post("/#{config.instagram_id}/media_publish", {
+        creation_id: container_id,
+        access_token: config.access_token
+      })
+      res[:id]
+    end
+
+    # Pseudo-public "API mode"
+    def get_media_permalink(media_id, config)
+      res = Http.get("/#{media_id}", {
+        fields: "permalink",
+        access_token: config.access_token
+      })
+      res[:permalink]
+    end
+
     private
 
     def retry_if_upload_times_out(times_remaining, post, options, &blk)
@@ -49,55 +122,35 @@ module Feed2Gram
       media = post.medias.first
 
       puts "Creating media resource for URL - #{media.url}" if options.verbose
-      container_id = Http.post("/#{config.instagram_id}/media", {
-        :media_type => post.media_type,
-        :caption => post.caption,
-        :access_token => config.access_token,
-        :cover_url => media.cover_url,
-        media.video? ? :video_url : :image_url => media.url
-      }.compact)[:id]
+      container_id = create_single_media_container(post, config)
 
       if media.video?
         wait_for_media_to_upload!(media.url, container_id, config, options)
       end
 
       puts "Publishing media for URL - #{media.url}" if options.verbose
-      Http.post("/#{config.instagram_id}/media_publish", {
-        creation_id: container_id,
-        access_token: config.access_token
-      })
+      publish_container(container_id, config)
       Result.new(post: post, status: :posted)
     end
 
     def publish_carousel(post, config, options)
       media_containers = post.medias.take(10).map { |media|
         puts "Creating media resource for URL - #{media.url}" if options.verbose
-        res = Http.post("/#{config.instagram_id}/media", {
-          :media_type => media.media_type,
-          :is_carousel_item => true,
-          :access_token => config.access_token,
-          media.video? ? :video_url : :image_url => media.url
-        }.compact)
-        res[:id]
+        create_carousel_media_container(media, config)
       }
-      post.medias.select(&:video?).zip(media_containers).each { |media, container_id|
-        wait_for_media_to_upload!(media.url, container_id, config, options)
+      post.medias.zip(media_containers).each { |media, container_id|
+        if media.video?
+          wait_for_media_to_upload!(media.url, container_id, config, options)
+        end
       }
 
       puts "Creating carousel media resource for post - #{post.url}" if options.verbose
-      carousel_id = Http.post("/#{config.instagram_id}/media", {
-        caption: post.caption,
-        media_type: post.media_type,
-        children: media_containers.join(","),
-        access_token: config.access_token
-      })[:id]
+      carousel_id = create_carousel_container(post, media_containers, config)
+
       wait_for_media_to_upload!(post.url, carousel_id, config, options)
 
       puts "Publishing carousel media for post - #{post.url}" if options.verbose
-      Http.post("/#{config.instagram_id}/media_publish", {
-        creation_id: carousel_id,
-        access_token: config.access_token
-      })
+      publish_container(carousel_id, config)
       Result.new(post: post, status: :posted)
     end
 
@@ -110,24 +163,16 @@ module Feed2Gram
           raise FacebookSucksAtDownloadingFilesError
         end
 
-        res = Http.get("/#{container_id}", {
-          fields: "status_code,status",
-          access_token: config.access_token
-        })
-        puts "Upload status #{res[:status_code]} after waiting #{wait_attempts * SECONDS_PER_UPLOAD_CHECK} seconds for IG to download #{url}" if options.verbose
-        if res[:status_code] == "FINISHED"
-          break
-        elsif res[:status_code] == "IN_PROGRESS"
-          wait_attempts += 1
-          sleep SECONDS_PER_UPLOAD_CHECK
-        else
-          warn <<~MSG
-            Unexpected status code (#{res[:status_code]}) uploading: #{url}"
-
-            API sent back this: #{res[:status]}
-
-            Error codes can be looked up here: https://developers.facebook.com/docs/instagram-platform/instagram-graph-api/reference/error-codes/
-          MSG
+        begin
+          puts "Uploading after waiting #{wait_attempts * SECONDS_PER_UPLOAD_CHECK} seconds for IG to download #{url}" if options.verbose
+          if upload_finished?(container_id, config)
+            break
+          else
+            wait_attempts += 1
+            sleep SECONDS_PER_UPLOAD_CHECK
+          end
+        rescue => e
+          warn e.message
           break
         end
       end
--- lib/feed2gram/refreshes_token.rb
+++ lib/feed2gram/refreshes_token.rb
@@ -1,10 +1,22 @@
 module Feed2Gram
   class RefreshesToken
     def refresh!(config, options)
+      puts "Refreshing Facebook OAuth token" if options.verbose
+      new_access_token = call(config)
+      return unless new_access_token
+
+      config.access_token = new_access_token
+      config.access_token_refreshed_at = Time.now.utc
+
+      puts "Updating Facebook OAuth token in: #{options.config_path}" if options.verbose
+      File.write(options.config_path, config.as_yaml)
+    end
+
+    # "API mode"
+    def call(config)
       return unless config.access_token_refreshed_at.nil? ||
         config.access_token_refreshed_at < Time.now - (60 * 60)
 
-      puts "Refreshing Facebook OAuth token" if options.verbose
       data = Http.get("/oauth/access_token", {
         grant_type: "fb_exchange_token",
         client_id: config.facebook_app_id,
@@ -12,11 +24,7 @@ module Feed2Gram
         fb_exchange_token: config.access_token
       })
 
-      config.access_token = data[:access_token]
-      config.access_token_refreshed_at = Time.now.utc
-
-      puts "Updating Facebook OAuth token in: #{options.config_path}" if options.verbose
-      File.write(options.config_path, config.as_yaml)
+      data[:access_token]
     end
   end
 end
